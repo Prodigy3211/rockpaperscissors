0c33a3680fee3e99250408dd7b633ecca2ae42c5
Prodigy3211 <37477839+Prodigy3211@users.noreply.github.com>
2024-05-21 17:32:45 -0400

    Updat for loop
    Using if statements it now checks user input for rock paper or scissors
    .

--- main.js
+++ main.js
@@ -4,24 +4,41 @@ const rock = 0;
 const paper = 1;
 const scissors = 2;
 const x = [rock,paper,scissors];
+let playerScore = 0;
+let compterScore = 0;
 
-function getComputerChoice(){
-    function getRandomInt(max){
-    return Math.floor(Math.random() * max);
-    }
-    return getRandomInt(3);
+function playerChoice(){
+    let y = prompt("Choose your weapon: ");
+        if (y === "rock"){
+            console.log ("You chose "+y);
+        } else if (y === "scissors"){
+            console.log ("You chose scissors!");
+        } else if (y === "paper"){
+            console.log("You chose "+y);
+        } else {
+            console.log("ERROR Please choose rock paper or scissors");
+        }
+
+}
+
+// function getComputerChoice(){
+//     function getRandomInt(max){
+//     return Math.floor(Math.random() * max);
+//     }
+//     return getRandomInt(3);
    
-}
+// }
 
-console.log(getComputerChoice());
+// // console.log(getComputerChoice());
 
-function incrementScore(){
-    if (getComputerChoice() === rock) {
-        console.log ("okay");
-    }
-    // console.log ("Rock");
-}
+// // function incrementScore(){
+// //     if (getComputerChoice() === rock) {
+        
+//     }
+//     // console.log ("Rock");
+// }
 
-console.log(incrementScore());
+playerChoice();
+// console.log(incrementScore());
 // const playerChoice = prompt ("Choose rock, paper, or scissors");
 // console.log("Your number is "+getRandomInt(3));
