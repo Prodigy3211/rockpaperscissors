dcae2f2129e55b249907c0d281e4a0a770a7c763
Prodigy3211 <37477839+Prodigy3211@users.noreply.github.com>
2024-05-21 13:53:02 -0400

    Update file with math function
    Add random generator for numbers

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 console.log("hello world");
+
+const playerChoice = prompt ("Choose rock, paper, or scissors");
+const rock = 0;
+const paper = 1;
+const scissors = 2;
+const x = [rock,paper,scissors];
+
+function getRandomInt(max){
+return Math.floor(Math.random() * max);
+    
+}
+
+// function getComputerChoice(){
+//     Math.random(x);
+//     return x;
+// }
+
+console.log("Your number is "+getRandomInt(3));
