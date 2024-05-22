2800c60a8131cb3db2380d64cdd1cff8041471e3
Prodigy3211 <37477839+Prodigy3211@users.noreply.github.com>
2024-05-22 14:48:21 -0400

    Update javascript
    Added some logic in for getting human and computer choices.

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 console.log("hello world");
 
-const rock = 0;
-const paper = 1;
-const scissors = 2;
-const x = [rock,paper,scissors];
+// const x = [rock,paper,scissors];
 let playerScore = 0;
 let compterScore = 0;
 
-function playerChoice(){
-    let y = prompt("Choose your weapon: ");
+function getPlayerChoice(){
+    let y = prompt("Choose your weapon (Rock, Paper, or Scissors): ").toLowerCase();
         if (y === "rock"){
             console.log ("You chose "+y);
         } else if (y === "scissors"){
@@ -16,19 +13,53 @@ function playerChoice(){
         } else if (y === "paper"){
             console.log("You chose "+y);
         } else {
-            console.log("ERROR Please choose rock paper or scissors");
+            console.log("ERROR Please choose rock, paper, or scissors");
         }
+        return y;
 
-}
+};
 
-// function getComputerChoice(){
-//     function getRandomInt(max){
-//     return Math.floor(Math.random() * max);
-//     }
-//     return getRandomInt(3);
+function getComputerChoice(){
+    function getRandomInt(max){
+    return Math.floor(Math.random() * max);
+    }
+    //Convert Int into Strings
+    let myWeapon = getRandomInt(3);
+    if(myWeapon === 0){
+        return "rock";
+    } else if (myWeapon === 1){
+        return "paper";
+    } else if (myWeapon === 2){
+        return "scissors";
+    }
    
+};
+
+// console.log(getRandomInt());
+
+// const result = function roundBattle(a,b){
+//         if (a === b){
+//             console.log ("It's a Tie game!");
+//         } else {
+//             console.log ("Error!");
+//         }
 // }
 
+
+function playRound(playerChoice,computerChoice){
+    if (playerChoice === computerChoice){
+        console.log("It's a Tie game!");
+    } else {
+        console.log("This better be working!");
+    }
+}
+
+const playerChoice = getPlayerChoice();
+const computerChoice = getComputerChoice();
+
+console.log(getPlayerChoice());
+console.log(getComputerChoice());
+playRound();
 // // console.log(getComputerChoice());
 
 // // function incrementScore(){
@@ -38,7 +69,8 @@ function playerChoice(){
 //     // console.log ("Rock");
 // }
 
-playerChoice();
+
+// console.log (result(playerChoice(),getComputerChoice()));
 // console.log(incrementScore());
 // const playerChoice = prompt ("Choose rock, paper, or scissors");
 // console.log("Your number is "+getRandomInt(3));
